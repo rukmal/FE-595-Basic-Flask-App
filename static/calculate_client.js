61fdbee98e8fda75e94b8ed1d901037cdaa3f70d
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-12-13 03:46:29 -0500

    Added javascript for page functionality, and made some cosmetic updates to the calculator page

--- static/calculate_client.js
+++ static/calculate_client.js
@@ -1,5 +1,29 @@
 console.log('hello world!')
 
+function makeRequest (endpoint, symbol) {
+    numberOne = $('#numberOne').val()
+    numberTwo = $('#numberTwo').val()
+    $.post(endpoint, {
+        numberOne,
+        numberTwo
+    }, function (response) {
+        solution_formatted = numberOne + ' ' + symbol + ' ' + numberTwo + ' = ' + response
+        $('#solutions').prepend('<p>' + solution_formatted + '</p>')
+    });
+}
+
 $('#add').click(function () {
-    console.log('it worked!')
+    makeRequest('/add', '+')
+});
+
+$('#subtract').click(function () {
+    makeRequest('/subtract', '-')
+});
+
+$('#divide').click(function () {
+    makeRequest('/divide', '÷')
+});
+
+$('#multiply').click(function () {
+    makeRequest('/multiply', '×')
 });
