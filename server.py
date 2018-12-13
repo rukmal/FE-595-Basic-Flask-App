49ba633d9a01ac77ba405a885a498f5ec6f1d433
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-12-13 03:12:22 -0500

    Updated server to calculate page

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/calculator', methods=['GET'])
+def calculateLandingPage():
+    return render_template('calculate.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
